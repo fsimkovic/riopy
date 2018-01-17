a72ccf1f45a0ae3a79d67f336c69c0c60b23d817
Felix Simkovic <felixsimkovic@me.com>
2018-01-17 13:00:08 +0000

    updates

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 #!/usr/bin/env ccp4-python
 
+from __future__ import print_function
+
+import logging
+import os
 import sys
 import time
 
 from iotbx import pdb
 from riopy.rio import Rio
+from riopy.misc import calculate_origin_shift, move_model_to_target
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def select(h, sel):
     return h.select(h.atom_selection_cache().selection(sel))
 
 
-def main():
-    inputf = sys.argv[1]
-    sminf = sys.argv[2]
+def main(mtz, target, model, debug=False):
 
-    ref = pdb.pdb_input(inputf)
+    if debug:
+        logger.setLevel(logging.DEBUG)
+
+    oshift = calculate_origin_shift(mtz, target, model)
+    logger.debug("Origin shift calculated to be: %s", oshift)
+    model = move_model_to_target(target, model, oshift)
+
+    ref = pdb.pdb_input(target)
     uc = ref.crystal_symmetry().unit_cell()
     sg = ref.crystal_symmetry().space_group()
 
     h = ref.construct_hierarchy()
-    s = pdb.pdb_input(sminf).construct_hierarchy()
+    s = pdb.pdb_input(model).construct_hierarchy()
 
     h_ca = select(h, "name ca")
     s_ca = select(s, "name ca")
 
     r = Rio(s_ca, h_ca, sg, uc)
-    print(r.compute(max_dist=1.5, cells=1))
+    rscore = r.compute(max_dist=1.5, cells=3)
+    logger.info("RIO score: %d", rscore)
+
+    if debug:
+        pass
+    else:
+        os.unlink(model)
+
+    return rscore
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", default=False, action="store_true")
+    parser.add_argument("mtz")
+    parser.add_argument("target")
+    parser.add_argument("model")
+    main(**vars(parser.parse_args()))
--- riopy/misc.py
+++ riopy/misc.py
@@ -1,3 +1,37 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+def calculate_origin_shift(mtz, target, model):
+    """Calculate the origin shift between a model and a target structure"""
+    from ample.util.shelxe import MRinfo
+    logger.debug("Calculating origin shift for %s given %s and %s", model, target, mtz)
+    mrinfo = MRinfo('shelxe', target, mtz)
+    mrinfo.analyse(model)
+    try:
+        map(os.remove, ["shelxe-input.ent", "shelxe-input.hkl"])
+    except:
+        pass
+    return mrinfo.originShift
+
+
+def move_model_to_target(target, model, origin_shift):
+    """Move a model to the target using ``csymmatch```"""
+    from ample.util.csymmatch import Csymmatch
+    logger.debug("Translating %s to %s with origin shift %s", target, model, origin_shift)
+    return Csymmatch().wrapModelToNative(model, target, origin=origin_shift)
+
+
+#  def move_model_to_target(target, model, origin_shift):
+#  """Move a model to the target using ``pdbcur``"""
+#      from ample.util.pdb_edit import translate
+#      out_file = "foobar.pdb"
+#      translate(inpdb=model, outpdb=out_file, ftranslate=origin_shift)
+#      return out_file
+
+
 def count_res_in_frag(residues, min_frag_len=3):
     """Count the number of fragments in a list
     
@@ -14,8 +48,8 @@ def count_res_in_frag(residues, min_frag_len=3):
        The number of fragments
 
     """
-
     if len(residues) < 1:
+        logger.debug("No residue indexes provided.")
         return 0
 
     def valid_frag_len(c):
